600f83c3bc573164a0fb9749aec1c33c22be637b
ledon <powertle@outlook.com>
2026-07-27 15:06:25 +0800

    ✨ feat(frontend, desktop): 优化前端构建配置、升级桌面端版本至0.1.8

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    assetsDir: '',
+  },
   server: {
     port: 5174,
     proxy: {
